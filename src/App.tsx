9353c9eedfd75b7f5b5248efeb0baaea213d03dc
Noel Schenk <schenknoel@gmail.com>
2024-01-28 15:31:35 +0100

    ud

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
    return (
     <>
-       
+    <div className='Toucher' onClick={() => {
+        document.querySelector('video')?.classList.toggle('TouchMe');
+        setTimeout(() => {
+          document.querySelector('video')?.classList.toggle('TouchMe');
+        }, 1000)
+       }}>
+       </div>
     </>
   )
 }
